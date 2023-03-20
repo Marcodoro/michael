c9c52b4f3d139d8b2b000d9cc79e339b9f94ce55
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2023-03-20 16:19:16 +0100

    sdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/analytics';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { useRef } from 'react';
-import { useState } from 'react';
+export default function App() {
+  const [text, setText] = useState('');
+  const [results, setResults] = useState([]);
 
-firebase.initializeApp({
-  apiKey: "AIzaSyA1e2n7falR5COK7U_QfZrBWJnxErySs2Q",
-  authDomain: "chat-app5665.firebaseapp.com",
-  projectId: "chat-app5665",
-  storageBucket: "chat-app5665.appspot.com",
-  messagingSenderId: "426300163873",
-  appId: "1:426300163873:web:179323da1eeea07c956060",
-  measurementId: "G-3ZS6ETF3ED"
-})
+  const handleClick = () => {
+    console.log(text);
+    setResult(text);
+  };
 
+  const setResult = (text) => {
+    const newResult = <h1 key={results.length}>{text}</h1>;
+    setResults([...results, newResult]);
+    setText('');
+  };
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-
-
-function App() {
-
-  const [user] = useAuthState(auth);
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13) { // 13 is the key code for "Enter" key
+      handleClick();
+    }
+  };
 
   return (
     <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <SignOut />
-      </header>
-
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-
+      {results}
+      <button onClick={handleClick}>Send</button>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} onKeyDown={handleKeyDown} />
     </div>
   );
-}
-
-function SignIn() {
-
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
-  return (
-    <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
-    </>
-  )
-
-}
-
-function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
-}
-
-
-function ChatRoom() {
-  const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const [formValue, setFormValue] = useState('');
-
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL
-    })
-
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  return (<>
-    <main>
-
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-      <span ref={dummy}></span>
-
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button type="submit" disabled={!formValue}>🕊️</button>
-
-    </form>
-  </>)
-}
-
-
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
-
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
-    </div>
-  </>)
 }
